ee46024c901a629507aa5e92b26292462ba66dbc
Fabian Gomez <fcgomez@unbosque.edu.co>
2022-09-03 16:27:17 -0500

    Actualizando PrincipalView

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -1,11 +1,22 @@
 package co.edu.unbosque.controller;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import co.edu.unbosque.view.PrincipalView;
 
 public class AplMain {
 
 	public static void main(String[] args) {
 
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			// UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel"); // NUEVO
+		} catch (UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException e) {
+		} catch (InstantiationException e) {
+		} catch (IllegalAccessException e) {
+		}
 		PrincipalView view = new PrincipalView();
 
 	}
--- src/co/edu/unbosque/view/PrincipalView.java
+++ src/co/edu/unbosque/view/PrincipalView.java
@@ -1,71 +1,112 @@
 package co.edu.unbosque.view;
 
+import java.awt.Color;
+
 import javax.swing.*;
 
-public class PrincipalView {
+public class PrincipalView extends JFrame {
 
-	JMenu menu, lista, binario, sql, nsql;
-	JMenuItem l1, l2, l3, l4, b1, b2, b3, b4, s1, s2, s3, s4, n1, n2, n3, n4;
+	private JMenuBar menubar;
+	private JMenu options;
+	private JMenuItem addPerson, editPerson, deletePerson, searchPerson, exit;
 
 	public PrincipalView() {
-		JFrame f = new JFrame("Menu and MenuItem Example");
-		JMenuBar mb = new JMenuBar();
-		menu = new JMenu("Menu");
+
+		setSize(800, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(new Color(244, 196, 196));
+
+		initializeComponents();
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
 		
-		lista = new JMenu("Lista");
-		binario = new JMenu("Binario");
-		sql = new JMenu("SQL");
-		nsql = new JMenu("NOSQL");
-		
-		l1 = new JMenuItem("Crear");
-		l2 = new JMenuItem("Borrar");
-		l3 = new JMenuItem("Editar");
-		l4 = new JMenuItem("Consultar");
-		
-		b1 = new JMenuItem("Crear");
-		b2 = new JMenuItem("Borrar");
-		b3 = new JMenuItem("Editar");
-		b4 = new JMenuItem("Consultar");
-		
-		s1 = new JMenuItem("Crear");
-		s2 = new JMenuItem("Borrar");
-		s3 = new JMenuItem("Editar");
-		s4 = new JMenuItem("Consultar");
-		
-		n1 = new JMenuItem("Crear");
-		n2 = new JMenuItem("Borrar");
-		n3 = new JMenuItem("Editar");
-		n4 = new JMenuItem("Consultar");
-		
-		lista.add(l1);
-		lista.add(l2);
-		lista.add(l3);
-		lista.add(l4);
-		
-		binario.add(b1);
-		binario.add(b2);
-		binario.add(b3);
-		binario.add(b4);
-		
-		sql.add(s1);
-		sql.add(s2);
-		sql.add(s3);
-		sql.add(s4);
-		
-		nsql.add(n1);
-		nsql.add(n2);
-		nsql.add(n3);
-		nsql.add(n4);
-		
-		menu.add(lista);
-		menu.add(binario);
-		menu.add(sql);
-		menu.add(nsql);
-		
-		mb.add(menu);
-		f.setJMenuBar(mb);
-		f.setSize(400, 400);
-		f.setLayout(null);
-		f.setVisible(true);
+	}
+
+	public void initializeComponents() {
+
+		menubar = new JMenuBar();
+		getContentPane().add(menubar);
+		setJMenuBar(menubar);
+
+		options = new JMenu("Options");
+		options.setMnemonic('O');
+		menubar.add(options);
+
+		addPerson = new JMenuItem("Add a person");
+		addPerson.setActionCommand("ADDPERSON");
+		options.add(addPerson);
+
+		editPerson = new JMenuItem("Edit a person");
+		editPerson.setActionCommand("EDITPERSON");
+		options.add(editPerson);
+
+		deletePerson = new JMenuItem("Delete a person");
+		deletePerson.setActionCommand("DELETEPERSON");
+		options.add(deletePerson);
+
+		searchPerson = new JMenuItem("Search a person");
+		searchPerson.setActionCommand("SEARCHPERSON");
+		options.add(searchPerson);
+
+		exit = new JMenuItem("Exit");
+		exit.setActionCommand("EXIT");
+		options.add(exit);
+	}
+
+	public JMenuBar getMenubar() {
+		return menubar;
+	}
+
+	public void setMenubar(JMenuBar menubar) {
+		this.menubar = menubar;
+	}
+
+	public JMenu getOptions() {
+		return options;
+	}
+
+	public void setOptions(JMenu options) {
+		this.options = options;
+	}
+
+	public JMenuItem getAddPerson() {
+		return addPerson;
+	}
+
+	public void setAddPerson(JMenuItem addPerson) {
+		this.addPerson = addPerson;
+	}
+
+	public JMenuItem getEditPerson() {
+		return editPerson;
+	}
+
+	public void setEditPerson(JMenuItem editPerson) {
+		this.editPerson = editPerson;
+	}
+
+	public JMenuItem getDeletePerson() {
+		return deletePerson;
+	}
+
+	public void setDeletePerson(JMenuItem deletePerson) {
+		this.deletePerson = deletePerson;
+	}
+
+	public JMenuItem getSearchPerson() {
+		return searchPerson;
+	}
+
+	public void setSearchPerson(JMenuItem searchPerson) {
+		this.searchPerson = searchPerson;
+	}
+
+	public JMenuItem getExit() {
+		return exit;
+	}
+
+	public void setExit(JMenuItem exit) {
+		this.exit = exit;
 	}
 }
