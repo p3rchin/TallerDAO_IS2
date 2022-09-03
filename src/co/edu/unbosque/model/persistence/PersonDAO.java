c68f6acb1f2d631fc3674a8a2cc9c9bfc197c12b
Fabian Gomez <fcgomez@unbosque.edu.co>
2022-09-03 16:09:27 -0500

    PersonDAO parcialmente terminado

--- src/co/edu/unbosque/model/persistence/PersonDAO.java
+++ src/co/edu/unbosque/model/persistence/PersonDAO.java
@@ -1,5 +1,64 @@
 package co.edu.unbosque.model.persistence;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import co.edu.unbosque.model.Person;
+
 public class PersonDAO {
 
+	ArrayList<Person> alPerson = new ArrayList<Person>();
+
+	public boolean searchPerson(String id) {
+		for (int i = 0; i < alPerson.size(); i++) {
+			if (id.equals(alPerson.get(i).getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean addPerson(Person person, String id) {
+
+		if (searchPerson(id) == false) {
+			alPerson.add(person);
+			return true;
+		} else {
+
+			return false;
+
+		}
+	}
+
+	public boolean editPerson(Person person, String id) {
+
+		for (int i = 0; i < alPerson.size(); i++) {
+			if (id.equals(alPerson.get(i).getId())) {
+
+				alPerson.get(i).setName(person.getName());
+				alPerson.get(i).setLastName(person.getLastName());
+				alPerson.get(i).setAge(person.getAge());
+				alPerson.get(i).setSex(person.getSex());
+				alPerson.get(i).setCountry(person.getCountry());
+				alPerson.get(i).setTelephone(person.getTelephone());
+				alPerson.get(i).setEmail(person.getEmail());
+
+				return true;
+
+			}
+		}
+		return false;
+	}
+
+	public boolean deletePerson(String id) {
+
+		for (int i = 0; i < alPerson.size(); i++) {
+			if (id.equals(alPerson.get(i).getId())) {
+
+				alPerson.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
 }
