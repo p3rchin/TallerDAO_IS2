862572981551c8a8fb5352f5023a6bd9c53d1a22
Fabian Gomez <fcgomez@unbosque.edu.co>
2022-09-03 18:31:38 -0500

    Agregando panel para añadir personas

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,38 @@
 package co.edu.unbosque.controller;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import co.edu.unbosque.view.PrincipalView;
+
+public class Controller implements ActionListener {
+
+	private PrincipalView principalView;
+
+	public Controller() {
+
+		principalView = new PrincipalView();
+		assignListeners();
+
+	}
+
+	public void assignListeners() {
+
+		principalView.getAddPersonArray().addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+
+		if (command.equals("ADDPERSONARRAY")) {
+
+			principalView.getAddPersonPanel().setVisible(true);
+			
+
+		}
+	}
 
 }
--- src/co/edu/unbosque/view/PrincipalView.java
+++ src/co/edu/unbosque/view/PrincipalView.java
@@ -6,9 +6,12 @@ import javax.swing.*;
 
 public class PrincipalView extends JFrame {
 
+	private AddPersonPanel addPersonPanel;
 	private JMenuBar menubar;
-	private JMenu options;
-	private JMenuItem addPerson, editPerson, deletePerson, searchPerson, exit;
+	private JMenu options, array, binary, sql, noSql;
+	private JMenuItem addPersonArray, editPersonArray, deletePersonArray, searchPersonArray, addPersonBinary,
+			editPersonBinary, deletePersonBinary, searchPersonBinary, addPersonSQL, editPersonSQL, deletePersonSQL,
+			searchPersonSQL, addPersonNoSQL, editPersonNoSQL, deletePersonNoSQL, searchPersonNoSQL, exit;
 
 	public PrincipalView() {
 
@@ -20,10 +23,13 @@ public class PrincipalView extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		
+
 	}
 
 	public void initializeComponents() {
+
+		addPersonPanel = new AddPersonPanel();
+		getContentPane().add(addPersonPanel);
 
 		menubar = new JMenuBar();
 		getContentPane().add(menubar);
@@ -33,25 +39,97 @@ public class PrincipalView extends JFrame {
 		options.setMnemonic('O');
 		menubar.add(options);
 
-		addPerson = new JMenuItem("Add a person");
-		addPerson.setActionCommand("ADDPERSON");
-		options.add(addPerson);
+		array = new JMenu("ArrayList");
+		array.setMnemonic('A');
+		options.add(array);
 
-		editPerson = new JMenuItem("Edit a person");
-		editPerson.setActionCommand("EDITPERSON");
-		options.add(editPerson);
+		binary = new JMenu("Binary file");
+		binary.setMnemonic('B');
+		options.add(binary);
 
-		deletePerson = new JMenuItem("Delete a person");
-		deletePerson.setActionCommand("DELETEPERSON");
-		options.add(deletePerson);
+		sql = new JMenu("SQL");
+		sql.setMnemonic('S');
+		options.add(sql);
 
-		searchPerson = new JMenuItem("Search a person");
-		searchPerson.setActionCommand("SEARCHPERSON");
-		options.add(searchPerson);
+		noSql = new JMenu("NoSQL");
+		noSql.setMnemonic('N');
+		options.add(noSql);
+
+		addPersonArray = new JMenuItem("Add a person");
+		addPersonArray.setActionCommand("ADDPERSONARRAY");
+		array.add(addPersonArray);
+
+		editPersonArray = new JMenuItem("Edit a person");
+		editPersonArray.setActionCommand("EDITPERSONARRAY");
+		array.add(editPersonArray);
+
+		deletePersonArray = new JMenuItem("Delete a person");
+		deletePersonArray.setActionCommand("DELETEPERSONARRAY");
+		array.add(deletePersonArray);
+
+		searchPersonArray = new JMenuItem("Search a person");
+		searchPersonArray.setActionCommand("SEARCHPERSONARRAY");
+		array.add(searchPersonArray);
+
+		addPersonBinary = new JMenuItem("Add a person");
+		addPersonBinary.setActionCommand("ADDPERSONBINARY");
+		binary.add(addPersonBinary);
+
+		editPersonBinary = new JMenuItem("Edit a person");
+		editPersonBinary.setActionCommand("EDITPERSONBINARY");
+		binary.add(editPersonBinary);
+
+		deletePersonBinary = new JMenuItem("Delete a person");
+		deletePersonBinary.setActionCommand("DELETEPERSONBINARY");
+		binary.add(deletePersonBinary);
+
+		searchPersonBinary = new JMenuItem("Search a person");
+		searchPersonBinary.setActionCommand("SEARCHPERSONBINARY");
+		binary.add(searchPersonBinary);
+
+		addPersonSQL = new JMenuItem("Add a person");
+		addPersonSQL.setActionCommand("ADDPERSONSQL");
+		sql.add(addPersonSQL);
+
+		editPersonSQL = new JMenuItem("Edit a person");
+		editPersonSQL.setActionCommand("EDITPERSONSQL");
+		sql.add(editPersonSQL);
+
+		deletePersonSQL = new JMenuItem("Delete a person");
+		deletePersonSQL.setActionCommand("DELETEPERSONSQL");
+		sql.add(deletePersonSQL);
+
+		searchPersonSQL = new JMenuItem("Search a person");
+		searchPersonSQL.setActionCommand("SEARCHPERSONSQL");
+		sql.add(searchPersonSQL);
+
+		addPersonNoSQL = new JMenuItem("Add a person");
+		addPersonNoSQL.setActionCommand("ADDPERSONNOSQL");
+		noSql.add(addPersonNoSQL);
+
+		editPersonNoSQL = new JMenuItem("Edit a person");
+		editPersonNoSQL.setActionCommand("EDITPERSONNOSQL");
+		noSql.add(editPersonNoSQL);
+
+		deletePersonNoSQL = new JMenuItem("Delete a person");
+		deletePersonNoSQL.setActionCommand("DELETEPERSONNOSQL");
+		noSql.add(deletePersonNoSQL);
+
+		searchPersonNoSQL = new JMenuItem("Search a person");
+		searchPersonNoSQL.setActionCommand("SEARCHPERSONNOSQL");
+		noSql.add(searchPersonNoSQL);
 
 		exit = new JMenuItem("Exit");
 		exit.setActionCommand("EXIT");
 		options.add(exit);
+	}
+
+	public AddPersonPanel getAddPersonPanel() {
+		return addPersonPanel;
+	}
+
+	public void setAddPersonPanel(AddPersonPanel addPersonPanel) {
+		this.addPersonPanel = addPersonPanel;
 	}
 
 	public JMenuBar getMenubar() {
@@ -70,36 +148,164 @@ public class PrincipalView extends JFrame {
 		this.options = options;
 	}
 
-	public JMenuItem getAddPerson() {
-		return addPerson;
+	public JMenu getArray() {
+		return array;
 	}
 
-	public void setAddPerson(JMenuItem addPerson) {
-		this.addPerson = addPerson;
+	public void setArray(JMenu array) {
+		this.array = array;
 	}
 
-	public JMenuItem getEditPerson() {
-		return editPerson;
+	public JMenu getBinary() {
+		return binary;
 	}
 
-	public void setEditPerson(JMenuItem editPerson) {
-		this.editPerson = editPerson;
+	public void setBinary(JMenu binary) {
+		this.binary = binary;
 	}
 
-	public JMenuItem getDeletePerson() {
-		return deletePerson;
+	public JMenu getSql() {
+		return sql;
 	}
 
-	public void setDeletePerson(JMenuItem deletePerson) {
-		this.deletePerson = deletePerson;
+	public void setSql(JMenu sql) {
+		this.sql = sql;
 	}
 
-	public JMenuItem getSearchPerson() {
-		return searchPerson;
+	public JMenu getNoSql() {
+		return noSql;
 	}
 
-	public void setSearchPerson(JMenuItem searchPerson) {
-		this.searchPerson = searchPerson;
+	public void setNoSql(JMenu noSql) {
+		this.noSql = noSql;
+	}
+
+	public JMenuItem getAddPersonArray() {
+		return addPersonArray;
+	}
+
+	public void setAddPersonArray(JMenuItem addPersonArray) {
+		this.addPersonArray = addPersonArray;
+	}
+
+	public JMenuItem getEditPersonArray() {
+		return editPersonArray;
+	}
+
+	public void setEditPersonArray(JMenuItem editPersonArray) {
+		this.editPersonArray = editPersonArray;
+	}
+
+	public JMenuItem getDeletePersonArray() {
+		return deletePersonArray;
+	}
+
+	public void setDeletePersonArray(JMenuItem deletePersonArray) {
+		this.deletePersonArray = deletePersonArray;
+	}
+
+	public JMenuItem getSearchPersonArray() {
+		return searchPersonArray;
+	}
+
+	public void setSearchPersonArray(JMenuItem searchPersonArray) {
+		this.searchPersonArray = searchPersonArray;
+	}
+
+	public JMenuItem getAddPersonBinary() {
+		return addPersonBinary;
+	}
+
+	public void setAddPersonBinary(JMenuItem addPersonBinary) {
+		this.addPersonBinary = addPersonBinary;
+	}
+
+	public JMenuItem getEditPersonBinary() {
+		return editPersonBinary;
+	}
+
+	public void setEditPersonBinary(JMenuItem editPersonBinary) {
+		this.editPersonBinary = editPersonBinary;
+	}
+
+	public JMenuItem getDeletePersonBinary() {
+		return deletePersonBinary;
+	}
+
+	public void setDeletePersonBinary(JMenuItem deletePersonBinary) {
+		this.deletePersonBinary = deletePersonBinary;
+	}
+
+	public JMenuItem getSearchPersonBinary() {
+		return searchPersonBinary;
+	}
+
+	public void setSearchPersonBinary(JMenuItem searchPersonBinary) {
+		this.searchPersonBinary = searchPersonBinary;
+	}
+
+	public JMenuItem getAddPersonSQL() {
+		return addPersonSQL;
+	}
+
+	public void setAddPersonSQL(JMenuItem addPersonSQL) {
+		this.addPersonSQL = addPersonSQL;
+	}
+
+	public JMenuItem getEditPersonSQL() {
+		return editPersonSQL;
+	}
+
+	public void setEditPersonSQL(JMenuItem editPersonSQL) {
+		this.editPersonSQL = editPersonSQL;
+	}
+
+	public JMenuItem getDeletePersonSQL() {
+		return deletePersonSQL;
+	}
+
+	public void setDeletePersonSQL(JMenuItem deletePersonSQL) {
+		this.deletePersonSQL = deletePersonSQL;
+	}
+
+	public JMenuItem getSearchPersonSQL() {
+		return searchPersonSQL;
+	}
+
+	public void setSearchPersonSQL(JMenuItem searchPersonSQL) {
+		this.searchPersonSQL = searchPersonSQL;
+	}
+
+	public JMenuItem getAddPersonNoSQL() {
+		return addPersonNoSQL;
+	}
+
+	public void setAddPersonNoSQL(JMenuItem addPersonNoSQL) {
+		this.addPersonNoSQL = addPersonNoSQL;
+	}
+
+	public JMenuItem getEditPersonNoSQL() {
+		return editPersonNoSQL;
+	}
+
+	public void setEditPersonNoSQL(JMenuItem editPersonNoSQL) {
+		this.editPersonNoSQL = editPersonNoSQL;
+	}
+
+	public JMenuItem getDeletePersonNoSQL() {
+		return deletePersonNoSQL;
+	}
+
+	public void setDeletePersonNoSQL(JMenuItem deletePersonNoSQL) {
+		this.deletePersonNoSQL = deletePersonNoSQL;
+	}
+
+	public JMenuItem getSearchPersonNoSQL() {
+		return searchPersonNoSQL;
+	}
+
+	public void setSearchPersonNoSQL(JMenuItem searchPersonNoSQL) {
+		this.searchPersonNoSQL = searchPersonNoSQL;
 	}
 
 	public JMenuItem getExit() {
